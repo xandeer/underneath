635508a11a5595d2b0e7dfc05011decad50c73ce
Kevin Du <kkxandeer@gmail.com>
2025-07-17 09:13:01 +0800

    Feat: Add SwiftUI View snapshot extension for UIKit platforms
    - Introduce a View extension to capture a SwiftUI View as a UIImage
    - Uses ImageRenderer and UIScreen scale to generate high-quality snapshots
    - Implementation is conditionally compiled for UIKit environments only

---
+++ Sources/Underneath/View+Exts.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  Underneath
+//
+//  Created by Kevin Du on 7/17/25.
+//
+
+#if canImport(UIKit)
+  import SwiftUI
+
+  extension View {
+    public func snapshot() -> UIImage? {
+      let r = ImageRenderer(content: self)
+      r.scale = UIScreen.main.scale
+      return r.uiImage
+    }
+  }
+#endif
