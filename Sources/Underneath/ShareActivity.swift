5550682296646d297a250984c4aada788eae9cd5
Kevin Du <kkxandeer@gmail.com>
2025-07-17 11:31:37 +0800

    Refactor: Remove onDisappear callback from ShareActivity show method
    - Simplify ShareActivity API by eliminating the onDisappear closure parameter and related delegate logic
    - Remove ActivityDelegate implementation and associated object handling
    - Focus show(for:) method strictly on presenting activity items for sharing

--- Sources/Underneath/ShareActivity.swift
+++ Sources/Underneath/ShareActivity.swift
@@ -15,7 +15,7 @@
 
   public struct ShareActivity {
     @MainActor
-    public static func show(for activityItems: [Any], onDisappear: (() -> Void)? = nil) {
+    public static func show(for activityItems: [Any]) {
       // Get root view controller
       guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
         let window = windowScene.windows.first,
@@ -30,16 +30,6 @@
         activityItems: activityItems,
         applicationActivities: nil
       )
-      if let onDisappear = onDisappear {
-        let delegate = ActivityDelegate(onDisappear: onDisappear)
-        activityVC.presentationController?.delegate = delegate
-        objc_setAssociatedObject(
-          activityVC,
-          Unmanaged.passUnretained(activityVC).toOpaque(),  // 用 activityVC 指针做 key
-          delegate,
-          .OBJC_ASSOCIATION_RETAIN_NONATOMIC
-        )
-      }
 
       // Configure for iPad
       if let popover = activityVC.popoverPresentationController {
@@ -54,18 +44,6 @@
       }
 
       rootViewController.present(activityVC, animated: true)
-    }
-  }
-
-  class ActivityDelegate: NSObject, UIAdaptivePresentationControllerDelegate {
-    let onDisappear: () -> Void
-
-    init(onDisappear: @escaping () -> Void) {
-      self.onDisappear = onDisappear
-    }
-
-    func presentationControllerDidDismiss(_ presentationController: UIPresentationController) {
-      onDisappear()
     }
   }
 #endif
