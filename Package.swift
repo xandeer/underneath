755a962e3ada4cd67a300acd7344f0f5f88e82aa
Kevin Du <kkxandeer@gmail.com>
2025-07-17 09:36:20 +0800

    Chore: Update minimum watchOS deployment target to v9 in Package.swift
    - Increase watchOS deployment target from v6 to v9 to ensure compatibility with newer APIs and features

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
   name: "Underneath",
   platforms: [
-    .macOS(.v10_15), .iOS(.v17), .tvOS(.v13), .watchOS(.v6), .visionOS(.v1),
+    .macOS(.v10_15), .iOS(.v17), .tvOS(.v13), .watchOS(.v9), .visionOS(.v1),
   ],
   products: [
     // Products define the executables and libraries a package produces, making them visible to other packages.
