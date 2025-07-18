70bbcc4ec8a002413f6f3399b5d9f05f3a1da66c
Kevin Du <kkxandeer@gmail.com>
2025-07-18 14:40:36 +0800

    Feat: Add Array extension for removing first matching object reference
    - Introduce Array+Exts.swift to provide a convenience method for removing the first occurrence of an object (by identity)
    - Useful for collections managing class instances where reference equality matters

---
+++ Sources/Underneath/Array+Exts.swift
@@ -0,0 +1,16 @@
+//
+//  Array+Exts.swift
+//  Underneath
+//
+//  Created by Kevin Du on 7/18/25.
+//
+
+import Foundation
+
+extension Array where Element: AnyObject {
+  mutating func removeFirst(object: Element) {
+    if let i = firstIndex(where: { $0 === object }) {
+      remove(at: i)
+    }
+  }
+}
