f129b44d787028e585aa9d97fdd0661fba67ff42
Kevin Du <kkxandeer@gmail.com>
2025-07-18 11:17:49 +0800

    Feat: Add ModelContext extensions for persistent model retrieval
    - Introduce extension methods on ModelContext for fetching models by PersistentIdentifier
    - Add Model struct to encapsulate persistent identifiers generically
    - Provide convenience initializer and retrieval methods to streamline fetching persistent models

---
+++ Sources/Underneath/ModelContext+Ext.swift
@@ -0,0 +1,50 @@
+//
+//  ModelContext+Ext.swift
+//  underneath
+//
+//  Created by Kevin Du on 5/26/25.
+//
+
+import Foundation
+import SwiftData
+
+extension ModelContext {
+  func persistentModel<T>(withID objectID: PersistentIdentifier) throws -> T?
+  where T: PersistentModel {
+    if let registered: T = registeredModel(for: objectID) {
+      return registered
+    }
+    if let notRegistered: T = model(for: objectID) as? T {
+      return notRegistered
+    }
+
+    var fetchDescriptor = FetchDescriptor<T>(
+      predicate: #Predicate { $0.persistentModelID == objectID }
+    )
+
+    fetchDescriptor.fetchLimit = 1
+
+    return try fetch(fetchDescriptor).first
+  }
+}
+
+public struct Model<T: PersistentModel>: Sendable {
+  public let persistentIdentifier: PersistentIdentifier
+
+  public init(persistentIdentifier: PersistentIdentifier) {
+    self.persistentIdentifier = persistentIdentifier
+  }
+}
+
+extension Model {
+  public init(_ model: T) {
+    self.init(persistentIdentifier: model.persistentModelID)
+  }
+}
+
+extension ModelContext {
+  func getOptional<T>(_ model: Model<T>) throws -> T?
+  where T: PersistentModel {
+    try self.persistentModel(withID: model.persistentIdentifier)
+  }
+}
