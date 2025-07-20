c4b9965aaf8ce28008155790fc1a89356c5e8f5b
Kevin Du <kkxandeer@gmail.com>
2025-07-20 14:33:37 +0800

    Feat: Add Haptic feedback utility class
    - Introduce a Haptic class for managing impact feedback in SwiftUI apps
    - Supports configurable feedback intervals and customizable feedback styles
    - Provides methods to trigger haptic feedback based on value changes or manual calls

---
+++ Sources/Underneath/Haptic.swift
@@ -0,0 +1,32 @@
+//
+//  Haptic.swift
+//
+//  Created by Kevin Du on 6/20/25.
+//
+
+import SwiftUI
+
+@MainActor
+public class Haptic {
+  private let hapticInterval: CGFloat
+  private let haptic: UIImpactFeedbackGenerator
+
+  private var lastHapticValue: CGFloat = 0
+
+  public init(hapticInterval: CGFloat, style: UIImpactFeedbackGenerator.FeedbackStyle) {
+    self.hapticInterval = hapticInterval
+    self.haptic = UIImpactFeedbackGenerator(style: style)
+    haptic.prepare()
+  }
+
+  public func triggerHapticIfNeeded(_ newValue: CGFloat, _ interval: CGFloat? = nil) {
+    if abs(lastHapticValue - newValue) > interval ?? hapticInterval {
+      haptic.impactOccurred()
+      lastHapticValue = newValue
+    }
+  }
+
+  public func impactOccurred() {
+    haptic.impactOccurred()
+  }
+}
