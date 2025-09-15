c738984508d28a104aaf168f6b77ec6d9ff62477
Kevin Du <kkxandeer@gmail.com>
2025-09-15 11:23:11 +0800

    Refactor: Simplify hide keyboard on tap implementation
    - Remove HideKeyboardOnTapModifier struct and directly handle keyboard hiding in the View extension
    - Reduce code duplication and improve clarity by delegating to fullTap closure in extension method

--- Sources/Underneath/SoftKeyboard.swift
+++ Sources/Underneath/SoftKeyboard.swift
@@ -7,22 +7,10 @@
 
 import SwiftUI
 
-public struct HideKeyboardOnTapModifier<FocusValue: Hashable>: ViewModifier {
-  private var focusedField: FocusState<FocusValue?>.Binding
-
-  public init(focusedField: FocusState<FocusValue?>.Binding) {
-    self.focusedField = focusedField
-  }
-
-  public func body(content: Content) -> some View {
-    content.fullTap {
-      focusedField.wrappedValue = nil
-    }
-  }
-}
-
 extension View {
   public func hideKeyboardOnTap<T: Hashable>(focusedField: FocusState<T?>.Binding) -> some View {
-    modifier(HideKeyboardOnTapModifier(focusedField: focusedField))
+    fullTap {
+      focusedField.wrappedValue = nil
+    }
   }
 }
